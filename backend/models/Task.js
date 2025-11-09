ed187bd9eb0c7996b226ec2bc6a0aebdb98af736
RoberthGodoy <jomeca13@gmail.com>
2025-11-09 00:38:14 -0600

    Modelos User y Task

---
+++ backend/models/Task.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const taskSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  description: String,
+  dueDate: Date,
+  category: { type: String, enum: ['Trabajo', 'Personal', 'Urgente', 'Otro'], default: 'Personal' },
+  completed: { type: Boolean, default: false },
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }
+}, { timestamps: true });
+
+module.exports = mongoose.model('Task', taskSchema);
